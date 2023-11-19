2eae69920d96247a01722646cbd5503fd3e134b1
phamhuuutin <phamhuutinhb@gmail.com>
2023-11-19 14:57:18 +0700

    update playiist router

--- projects/app-admin/src/app/modules/playlists/playlist-routing.module.ts
+++ projects/app-admin/src/app/modules/playlists/playlist-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlaylistsComponent } from './components/playlist/playlists/playlists.component';
+import { PlaylistComponent } from './playlist/playlist.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PlaylistsComponent,
+  },
+  {
+    path: ':file',
+    component: PlaylistComponent,
   },
 ];
 
