5573bde2828f3a9f6928a7a2c8e5db68e95f94e2
PhanQuanMacM2 <phanquan.2401@gmail.com>
2023-12-13 12:01:39 +0700

    add cicd for uat

--- scripts/deploy_uat_cache.sh
+++ scripts/deploy_uat_cache.sh
@@ -1,3 +1,3 @@
-docker login adview.cxview.ai
+docker login adview.cxview.ai -u AdView -p adview@2023
 docker build -t adview.cxview.ai/lpdev/dsd/dsd-frontend-cache-uat:latest -f ./docker/Dockerfile_cache .
 docker push adview.cxview.ai/lpdev/dsd/dsd-frontend-cache-uat:latest
--- scripts/deploy_uat_frontend.sh
+++ scripts/deploy_uat_frontend.sh
@@ -1,3 +1,3 @@
-docker login adview.cxview.ai
+docker login adview.cxview.ai -u AdView -p adview@2023
 docker build -t adview.cxview.ai/lpdev/dsd/dsd-frontend-uat:latest -f ./docker/Dockerfile_uat .
 docker push adview.cxview.ai/lpdev/dsd/dsd-frontend-uat:latest
