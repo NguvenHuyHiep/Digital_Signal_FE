2e40d8c7c89380729b101eb1e17695c456c93c0b
PhanQuanMacM2 <phanquan.2401@gmail.com>
2023-12-18 16:46:37 +0700

    fix device group

--- projects/app-admin/src/app/modules/device-group/components/device-group/device-group-add/device-group-add.component.ts
+++ projects/app-admin/src/app/modules/device-group/components/device-group/device-group-add/device-group-add.component.ts
@@ -49,43 +49,19 @@ export class DeviceGroupAddComponent implements OnInit {
   tableColumns: ColumnItem<Device>[] = [
     {
       name: 'module.device.code',
-      sortOrder: 'descend',
-      sortFn: (a: Device, b: Device) =>
-        a.code?.localeCompare(b.code as string) as number,
-      listOfFilter: [],
-      filterFn: null,
-      filterMultiple: false,
-      sortDirections: ['ascend', 'descend', null],
+      key: 'code',
     },
     {
       name: 'module.device.name',
-      sortOrder: 'descend',
-      sortFn: (a: Device, b: Device) =>
-        a.name?.localeCompare(b.name as string) as number,
-      listOfFilter: [],
-      filterFn: null,
-      filterMultiple: false,
-      sortDirections: ['ascend', 'descend', null],
+      key: 'name',
     },
     {
       name: 'module.device.info',
-      sortOrder: 'descend',
-      sortFn: (a: Device, b: Device) =>
-        a.information?.localeCompare(b.information as string) as number,
-      listOfFilter: [],
-      filterFn: null,
-      filterMultiple: false,
-      sortDirections: ['ascend', 'descend', null],
+      key: 'information',
     },
     {
       name: 'module.device.status',
-      sortOrder: 'descend',
-      sortFn: (a: Device, b: Device) =>
-        a.status?.localeCompare(b.status as string) as number,
-      listOfFilter: [],
-      filterFn: null,
-      filterMultiple: false,
-      sortDirections: ['ascend', 'descend', null],
+      key: 'status',
     },
   ];
 
