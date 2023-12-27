53365ca58ee8ebe39804d3c0ee9fa8da17b0863d
impAct <annguyenxax@gmail.com>
2023-12-27 09:06:27 +0700

    remove unused code

--- projects/app-admin/src/app/modules/device-group/components/device-group/device-group-add/device-group-add.component.ts
+++ projects/app-admin/src/app/modules/device-group/components/device-group/device-group-add/device-group-add.component.ts
@@ -65,33 +65,33 @@ export class DeviceGroupAddComponent implements OnInit {
     },
   ];
 
-  tableConfig: LhTableConfigModel = {
-    key: 'id',
-    disableDetail: true,
-    disableUpdate: true,
-    fields: [
-      {
-        label: 'module.device.code',
-        field: 'code',
-        type: LhTableFieldType.STRING,
-      },
-      {
-        label: 'module.device.name',
-        field: 'name',
-        type: LhTableFieldType.STRING,
-      },
-      {
-        label: 'module.device.info',
-        field: 'information',
-        type: LhTableFieldType.STRING,
-      },
-      {
-        label: 'module.device.status',
-        field: 'status',
-        type: LhTableFieldType.STRING,
-      },
-    ],
-  };
+  // tableConfig: LhTableConfigModel = {
+  //   key: 'id',
+  //   disableDetail: true,
+  //   disableUpdate: true,
+  //   fields: [
+  //     {
+  //       label: 'module.device.code',
+  //       field: 'code',
+  //       type: LhTableFieldType.STRING,
+  //     },
+  //     {
+  //       label: 'module.device.name',
+  //       field: 'name',
+  //       type: LhTableFieldType.STRING,
+  //     },
+  //     {
+  //       label: 'module.device.info',
+  //       field: 'information',
+  //       type: LhTableFieldType.STRING,
+  //     },
+  //     {
+  //       label: 'module.device.status',
+  //       field: 'status',
+  //       type: LhTableFieldType.STRING,
+  //     },
+  //   ],
+  // };
 
   form: FormDeviceGroup = this.adminDeviceGroupService.buildDeviceGroupForm(
     this.currentDeviceGroup
@@ -141,7 +141,10 @@ export class DeviceGroupAddComponent implements OnInit {
             this.message.error(errorsInStr);
           }
         },
-        error: (err) => {},
+        error: (err) => {
+          this.message.error('Error', err);
+          this.loading.searching = false;
+        },
         complete: () => {
           this.loading.searching = false;
         },
